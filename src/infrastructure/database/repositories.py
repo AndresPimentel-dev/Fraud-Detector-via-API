bd6d5903483eab27e586eedf4c3df7405fcebfae
Andres Pimentel <pimentelandressantiago@gmail.com>
2026-06-19 16:40:14 -0500

    database

--- src/infrastructure/database/repositories.py
+++ src/infrastructure/database/repositories.py
@@ -15,7 +15,7 @@ class UserRepository(UserRepositoryInterface):
             return None
         return db_user
     def create_user(self, username: str, hashed_password: str):
-        new_user = UsersTable(username= username, Hashed_password=hashed_password)
+        new_user = UsersTable(username= username, hashed_password=hashed_password)
 
         self.db.add(new_user)
         self.db.commit()
